ed00e10819d234bcb2df4ccbfdb7f61190d1eac4
Joshua Maciak <joshua.maciak@gmail.com>
2017-08-14 23:22:50 -0400

    implemented rtp fixed header & header extension structures

--- ez_rtp.h
+++ ez_rtp.h
@@ -1,4 +1,48 @@
-#ifndef EZ_RTP_H
-#define EZ_RTP_H
-// todo add code
+#ifndef RTP_H
+#define RTP_H
+/**
+ *				Real-time Transport Protocol
+ * Author: Joshua Maciak
+ * Date: August 13 2017
+ *
+ * The purpose of this library is to provide an implementation of the 
+ * Real-time Transport Protocol (RTP) & the Real-time Transport Control 
+ * Protocol (RTCP) based off the protocols outlined in RFC 3550.
+ * 
+ * Ideally, this library will implement the entirety of the standard.
+ * However, the functionality of this library will be implemented on
+ * an as-needed basis. As a result, things may not work/be as robust
+ * as the specification. In addition, things may change quickly as issues 
+ * are discovered.
+ *  
+**/
+#include <stdint.h>
+
+#define RTP_VERSION 2
+
+/**
+ * A bit field used to represent the fixed-length RTP Header.
+**/
+struct rtp_header_fixed {
+	unsigned int version: 2;          // RTP version
+	unsigned int padding: 1;          // indicates whether the header contains padding
+	unsigned int extension: 1;        // indicates whether the header contains an extension
+	unsigned int csrc_count: 4;       // the number of csrc id's that follow the fixed header
+	unsigned int marker: 1;           // determined by the RTP profile
+	unsigned int payload_type: 7;     // identifies the format of the payload 
+	unsigned int sequence_number: 16; // the number of the rtp packet sent in the sequence. increment by 1 for each packet
+	unsigned int timestamp: 32;	  // the timestamp of the packet
+	unsigned int ssrc: 32;		  // the synchronization source  
+	// list of CSRC's (32 bits) size csrc_count should be placed directly after this fixed header. 
+};
+
+/**
+ * Represents an RTP header extension
+**/
+struct rtp_header_extension {
+	uint16_t profile_specific; 	// 16 bits to be defined by the profile for anything
+	uint16_t length;		// 16 bits - number of words (4 bytes) in extension (excluding this header)
+	uint32_t* data; 		// the extension data
+};
+
 #endif
