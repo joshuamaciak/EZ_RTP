cc3e2ce6885b6151dc16dbff4643d009369228b1
Joshua Maciak <joshua.maciak@gmail.com>
2017-08-17 16:49:55 -0400

    added new module for socket & networking helper functions

--- Makefile
+++ Makefile
@@ -18,14 +18,18 @@ $(EXEC_OUTPUTNAME): ez_rtp_tester.c $(LIB_OUTPUTNAME)
 	@mkdir -p $(EXEC_DIR)
 	$(CC) -o $(EXEC_DIR)/$@ $< $(EXEC_LDFLAGS)
 
-$(LIB_OUTPUTNAME): ez_rtp.o
+$(LIB_OUTPUTNAME): ez_rtp.o ez_network.o
 	@mkdir -p $(LIB_DIR)
-	ar rcs $(LIB_DIR)/$@ $(OBJ_DIR)/$<
+	cd $(OBJ_DIR) && \
+	ar rcs ../$(LIB_DIR)/$@ $^
 
 ez_rtp.o: ez_rtp.c ez_rtp.h	
 	@mkdir -p $(OBJ_DIR)
-	$(CC) -c -o $(OBJ_DIR)/$@ $<
+	$(CC) -c $< -o $(OBJ_DIR)/$@
 
+ez_network.o: ez_network.c ez_network.h
+	@mkdir -p $(OBJ_DIR)
+	$(CC) -c $< -o $(OBJ_DIR)/$@
 #ez_rtp_tester.o: ez_rtp_tester.c
 #	$(CC) -o $@ $^ $(LDFLAGS)
 
---
+++ ez_network.h
@@ -0,0 +1,17 @@
+#ifndef EZ_NETWORK_H
+#define EZ_NETWORK_H
+/**
+ * EZ_NETWORK  
+ * Author: Joshua Maciak
+ * Date: August 13 2017
+ * 
+ * EZ_NETWORK is a header full of various convenience functions
+ * for networking. This header will contain a bunch of functions
+ * that will take a lot of the leg-work out of communicating via 
+ * sockets, which will mean less messy error-prone code in the EZ_RTP
+ * library. It also provides a sense of module-ization. If later I need
+ * to add something like packet-level encryption (DTLS) via OpenSSL,
+ * I can simply switch out the implementations (in theory)
+**/
+
+#endif
