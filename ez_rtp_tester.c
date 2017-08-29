06cde4b404b9c5aeb6ea51b007b87cbfd1f71b4b
Joshua Maciak <jtm97@pitt.edu>
2017-08-28 20:07:33 -0400

    updated tester code

--- ez_rtp_tester.c
+++ ez_rtp_tester.c
@@ -1,7 +1,189 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include "ez_rtp.h"
+#include <inttypes.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/stat.h>
+#include <string.h>
 
+#define RTP_PAYLOAD_MPEG4 34
+
+int is_rtp_header_valid();
+void print_rtp_packet(struct rtp_packet* rtp_packet, size_t packet_size_bytes);
+void print_rtp_session(struct rtp_session* session);
+int rtp_header_to_n(struct rtp_header* src, struct rtp_header* dest);
+void read_nal_bitstream_send(struct rtp_session* session, char* filename, int port);
+void fpeek(void* buf, int bytes, FILE* file);
 int main(int argc, char** argv) {
-	printf("Hello, RTP!\n");
+	/*struct rtp_profile profile;
+	profile.uses_extension 	 = 0;
+	profile.payload_type	 = RTP_PAYLOAD_MPEG4;
+	profile.max_payload_length = 0;
+	
+	struct rtp_packet my_first_packet;
+	my_first_packet.header.bitfields  = VERSION_MASK(2) | PADDING_MASK(0) | EXTENSION_MASK(0) | CSRC_COUNT_MASK(1) | MARKER_MASK(0) | PAYLOAD_TYPE_MASK(RTP_PAYLOAD_MPEG4);
+	my_first_packet.header.csrc[0]    = 69696969;
+	my_first_packet.header.ssrc       = 69696969;
+	my_first_packet.payload[0]	  = 69;
+	printf("bitfields:%" PRIu16 "\n", my_first_packet.header.bitfields);	
+	int res = 0;
+	res = is_rtp_header_valid(&my_first_packet, sizeof(struct rtp_packet), sizeof(struct rtp_header), &profile);
+	print_rtp_packet(&my_first_packet, sizeof(struct rtp_packet));
+	if(res == 1) {
+		printf("RTP header is valid!!\n");
+	} else {
+		printf("Error: invalid RTP header.\n");
+	}*/
+	struct rtp_session my_session;
+	if(rtp_session_init(&my_session) == 0) {
+		printf("Error: failed to initialize RTP session.\n");
+		return -1;
+	}
+	printf("Successfully initiated RTP session.\n");
+	print_rtp_session(&my_session);
+	
+	/*struct rtp_header be_header;
+	rtp_header_to_n(&(my_first_packet.header), &be_header); 
+	if(ez_sendto(my_session.rtp_sock, (void*) &be_header, sizeof(struct rtp_header), AF_INET, "127.0.0.1", 55555) == 1) {
+		printf("Sent!\n");
+	}*/
+	if(argc == 2 && strcmp(argv[1], "r") == 0) {
+		printf("Waiting for rtp packets...\n");
+	while(1) {
+			uint8_t* buf = NULL;
+			size_t length = 1000;
+			if(ez_recv_noblock(my_session.rtp_sock, &buf, &length) == 1) {
+				printf("Received RTP packet. size:%zu", length);
+			}
+			free(buf);
+		}
+		return 0;
+	} else if(argc == 3 && strcmp(argv[1], "s") == 0) {
+		
+		read_nal_bitstream_send(&my_session, "out.264", atoi(argv[2]));
+	}	
 	return 0;
+}
+/**
+ * Converts fields of rtp_header to network byte ordering
+**/
+int rtp_header_to_n(struct rtp_header* dest, struct rtp_header* const src) {
+	*dest = *src;
+	dest->bitfields = htons(src->bitfields);
+	dest->sequence_number = htons(src->sequence_number);
+	dest->timestamp	      = htonl(src->timestamp);
+	dest->ssrc	      = htonl(src->ssrc);
+	for(int i = 0; i < GET_CSRC_COUNT(src->bitfields); ++i) {
+		dest->csrc[i] = htonl(src->csrc[i]);
+	}
+	return 1;
+}
+/**
+ * reads a nal bitstream & sends it via rtp
+**/
+void read_nal_bitstream_send(struct rtp_session* session, char* filename, int port) {
+	FILE* bitstream = fopen(filename, "rb");
+	struct stat buf;
+	fstat(fileno(bitstream), &buf);
+	int file_size = buf.st_size;
+	if(bitstream == NULL) {
+		printf("Invalid file.\n");
+		return;
+	}
+	uint8_t start_code_prefix[] = {0x0, 0x0, 0x0, 0x1};
+	uint8_t stack[4];
+	uint8_t nal_buf[50000];
+	size_t max_nal_buf_size = 50000;
+	size_t cur_nal_buf_size = 0;	
+	int start_nal = 1;
+	uint8_t cur_byte;
+	memset(nal_buf, 0, sizeof(uint8_t) * max_nal_buf_size);
+	
+	for(int i = 0; i < file_size; ++i) {
+		fread(&cur_byte, sizeof(uint8_t), 1, bitstream);
+		if(start_nal == 1) {
+			nal_buf[cur_nal_buf_size] = cur_byte;
+			cur_nal_buf_size++;
+		}
+		stack[0] = stack[1];
+		stack[1] = stack[2];
+		stack[2] = stack[3];
+		stack[3] = cur_byte;
+		if((stack[0] == 0x0 && stack[1] == 0x0 && stack[2] == 0x0 && stack[3] == 0x1) || (stack[1] == 0x0 && stack[2] == 0x0 && stack[3] == 0x1)) {
+			start_nal = 1;
+			if(cur_nal_buf_size == 0) continue;
+			struct rtp_header header;
+			header.bitfields = VERSION_MASK(2) | PADDING_MASK(0) | EXTENSION_MASK(0) | CSRC_COUNT_MASK(1) | MARKER_MASK(0) | PAYLOAD_TYPE_MASK(RTP_PAYLOAD_MPEG4);
+			header.csrc[0] = 0x001111;
+			header.ssrc = 0x001010;			
+			// todo: need to adjust based on csrc list size
+			size_t packet_size = sizeof(struct rtp_header) + cur_nal_buf_size;
+			printf("NAL unit length: %zu packet header: %zu total packet size: %zu\n", cur_nal_buf_size, sizeof(struct rtp_header), packet_size);			// send data
+			struct rtp_packet* new_packet = malloc(packet_size);
+			memset(new_packet, 0, sizeof(uint8_t) * packet_size);
+			memcpy(new_packet->payload, nal_buf, cur_nal_buf_size);
+			rtp_header_to_n(&(new_packet->header), &header); 
+        		if(ez_sendto(session->rtp_sock, (void*) new_packet, packet_size, AF_INET, "127.0.0.1", port) == 1) {
+                		printf("Sent!\n");
+        		}			 
+			cur_nal_buf_size = 0;
+		}
+	}
+
+}
+void fpeek(uint8_t* buf, int bytes, FILE* file) {
+	fread(buf, sizeof(uint8_t), bytes, file);
+	fseek(file, -bytes, SEEK_CUR);	
+}
+void print_rtp_session(struct rtp_session* session) {
+	printf("- RTP Session - \n");
+	printf("SSRC:%" PRIu32 "\nRTP port:%d\nRTCP port:%d\nParticipants:%d\n", session->ssrc, session->rtp_port, session->rtcp_port, session->num_participants);	
+}
+void print_rtp_packet(struct rtp_packet* packet, size_t packet_size_bytes) {
+	struct rtp_header header = packet->header;
+	printf("- RTP packet - \n");
+	printf("Size: %ld octets\n", packet_size_bytes);
+	printf("* Header *\n");
+	printf("Size: %ld octets\n", sizeof(struct rtp_header) + (GET_CSRC_COUNT(header.bitfields) - 1) * 4);
+	printf("version:%d\npadding:%d\nextension:%d\ncsrc_count:%d\nmarker:%d\npayload_type:%d\nsequence_number:%d\ntimestamp:%d\nssrc:%d\n", GET_VERSION(header.bitfields), GET_PADDING(header.bitfields), GET_EXTENSION(header.bitfields), GET_CSRC_COUNT(header.bitfields), GET_MARKER(header.bitfields), GET_PAYLOAD_TYPE(header.bitfields), header.sequence_number, header.timestamp, header.ssrc);
+	for(int i = 0; i < GET_CSRC_COUNT(header.bitfields); ++i) printf("csrc[%d]: %d\n", i, header.csrc[i]);
+}
+
+/**
+ * Checks to see if an RTCP packet is valid (note: a packet can consist 
+ * of multiple packets, so all must be sanitized)
+**/
+
+int is_rtcp_packet_valid(struct rtcp_packet* packet, int packet_size_words) {
+	// version must be 2
+	if(packet->header.version != RTP_VERSION) return 0;
+	// first packet must be SR or RR
+	if((packet->header.packet_type != RTCP_PACKET_SENDER_REPORT) || (packet->header.packet_type != RTCP_PACKET_RECEIVER_REPORT)) {
+		return 0;
+	}	
+	// first packet shouldn't have padding
+	if(packet->header.padding == 1) return 0;
+	// ensure individual packet length adds up to total packet size
+	struct rtcp_packet* cur = packet;
+	struct rtcp_packet* end = (struct rtcp_packet*) ((uint32_t*) packet + packet_size_words);
+	do cur = (struct rtcp_packet*) ((uint32_t *)cur + cur->header.length + 1);
+	while(cur < end && cur->header.version == RTP_VERSION);
+	if(cur != end) return 0;
+	return 1;	
+}
+
+/**
+ * Checks to see if the packet contains a valid RTP header
+**/
+int is_rtp_header_valid(struct rtp_packet* packet, int packet_size, int header_size, struct rtp_profile* profile) {
+	struct rtp_header header = packet->header;
+	if(GET_VERSION(header.bitfields) != RTP_VERSION) return 0;
+	if(GET_PAYLOAD_TYPE(header.bitfields) != profile->payload_type) return 0;
+	if(GET_EXTENSION(header.bitfields) != profile->uses_extension) return 0;
+	if(GET_PADDING(header.bitfields) == 1) {
+		uint8_t num_padding_octets = *((uint8_t*)packet + (packet_size - 1));
+		if(num_padding_octets >= packet_size - header_size) return 0;
+	}
+	return 1;	 
 }
